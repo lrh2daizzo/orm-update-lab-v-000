49041c6dd3ccfca6b285f19342b85471d5f28e7e
Luis Herrera <herrera.lrh@gmail.com>
2020-04-08 16:52:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
       WHERE name = ?;
     SQL
 
-    DB[:conn].execute(sql, self.name).map { |row| self.new_from_db(row) }.first
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
     binding.pry
   end
 
