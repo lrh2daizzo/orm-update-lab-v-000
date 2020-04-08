70cfd668c3bb6b43633980f7a077fec08d7e7110
Luis Herrera <herrera.lrh@gmail.com>
2020-04-08 16:53:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
       WHERE name = ?;
     SQL
 
-    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
+    DB[:conn].execute(sql, name)
   end
 
   # Remember, you can access your database connection anywhere in this class
